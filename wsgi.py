4bd60bc3248a61eadc9f1fc96b7e9821212e20d2
nguacon01 <dungdm6191@gmail.com>
2021-02-05 23:04:38 +0100

    prune

--- wsgi.py
+++ wsgi.py
@@ -1,44 +1,21 @@
-from app.Jobs import DONE_JOBS
-from flask import Flask, redirect, url_for, request, render_template
-import time
-from datetime import datetime, timedelta
-from xml.sax import handler, make_parser
-import glob
-import os
-import os.path as op
-import sys
-import json
-try:
-    from uptime import uptime # installed uptime module at Pasteur
-except:
-    print ("no uptime module")
-from subprocess import check_output
-
-from configparser import ConfigParser
+from flask import Flask
 
 import app as queueManager
 
 app = Flask(__name__)
 
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-# mini WEB server for QueueManager
-
-# see http://bottle.paws.de/docs/dev/index.html
-
-# Created by Marc-Andre on 2015-4-14 based on an early version 2011-01-26.
-
-# This code is Licenced under the Cecill licence code
-
-# """
-
 @app.route('/')
 @app.route('/index')
 def index():
-    queue = queueManager.get_QM()
-    return queue
+    queueManager.get_QM()
+
+def email():
+    # import yagmail
+    # yag = yagmail.SMTP('dungdm91@gmail.com',"mjgvaophvtmvoljm")
+    # yag.send('dungdm6191@gmail.com', subject = None, contents = 'Hello')
+    return True
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9999, debug=True)
+    index()
+    # app.run(host='0.0.0.0', port=9999, debug=True)
