1b484d3d0a6d41b05cbb830ec571a5cd38d3a431
nguacon01 <dungdm6191@gmail.com>
2021-02-05 23:30:06 +0100

    fix loi not sending mail

--- app/Mail.py
+++ app/Mail.py
@@ -1,31 +1,33 @@
 import smtplib, ssl
-# import yagmail
+import yagmail
+import logging
 
-port = 465  # For SSL
+port = 25  # For SSL
 
 class Email(object):
-    def __init__(self, config, receiver, body):
+    def __init__(self, config, receiver, body, subject):
         self.config = config
         self.receiver = receiver
         self.body = body
         self.sender = self.config.get('QMServer', 'SendMail')
+        self.subject = subject
 
     def sendMail(self):
         # Create a secure SSL context
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-            server.login(self.sender, self.config.get('QMServer', 'SecKey'))
-            server.sendmail(from_addr=self.sender, to_addrs=self.receiver, msg=self.body)
-        # receiver = self.receiver
-        # body = self.body
+        # context = ssl.create_default_context()
+        # with smtplib.SMTP_SSL("smtp-relay.gmail.com", port, context=context) as server:
+        #     try:
+        #         server.login(self.sender, self.config.get('QMServer', 'SecKey'))
+        #         server.sendmail(from_addr=self.sender, to_addrs=self.receiver, msg=self.body)
+        #         logging.info('email is sended')
+        #     except:
+        #         logging.info('email is not sended')
+        receiver = self.receiver
+        body = self.body
 
-        # yag = yagmail.SMTP("dungdm91@gmail.com", "xxxxx")
-        # yag.send(
-        #     to=receiver,
-        #     subject="Yagmail test with attachment",
-        #     contents=body
-        # )
-
-
-
-
+        yag = yagmail.SMTP(self.config.get('QMServer', 'SendMail'), self.config.get('QMServer', 'SecKey'))
+        yag.send(
+            to=receiver,
+            subject=self.subject,
+            contents=body
+        )
