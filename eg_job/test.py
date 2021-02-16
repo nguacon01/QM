44f7c2b9c3c1da4dbc43c650ec1778a304acb7fc
MAD <madelsuc@unistra.fr>
2021-02-16 18:47:52 +0100

    improved the eg_job

--- eg_job/test.py
+++ eg_job/test.py
@@ -1,12 +1,19 @@
 import sys
 import time
-sys.path.insert(0,'spike.zip')    # to show how a big project can be included
-from spike.NPKConfigParser import NPKConfigParser
+sys.path.insert(0,'mymodule.zip')    # to show how a big project can be included
+try:
+    from mymodule import MyConfigParser
+except ModuleNotFoundError:
+    # fallback if you do not have mymodule installed or copied here
+    from configparser import ConfigParser as MyConfigParser
+
 
 '''
 Dummy processing,  as a test for QM
 '''
 def PROC(config):
+    print("Fake processing\nInitializing...")
+    time.sleep(1)
     size = config.getint("Proc","Size")
     total = 0
     for i in range(size):
@@ -19,6 +26,7 @@ def PROC(config):
     print ("Done")
 if __name__=='__main__':
     configfile = sys.argv[1]
-    config = NPKConfigParser()
+    config = MyConfigParser()
     config.read(configfile)
     processing = PROC(config)
+
